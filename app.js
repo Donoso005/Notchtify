87a31d7f298dd73742295c02c1b45d7002c64959
Alvaro Donoso <donoso005.developer@gmail.com>
2023-03-23 15:50:05 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const figlet = require('figlet')
+const app = express();
+require('dotenv').config();
+
+const port = parseInt(process.env.APP_PORT);
+
+app.get('/', (req, res) =>{
+    res.render('index')
+})
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
+
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!")
+})
+
+app.listen(port, () => {
+    figlet('Notchtify', function(err, data) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        console.log(data)
+    });
+    console.log(`-> Notchtify listening on port ${port}, have fun :) URL: http://localhost:${port}. Made by Don_oso005`)
+})
---
+++ public/js/data.js
@@ -0,0 +1,38 @@
+let songs = [
+  {
+    "name": "Aengus",
+    "path": "../audio/Aengus.mp3",
+    "artist": "Don_oso005",
+    "cover": ""
+},
+{
+    "name": "Aereda",
+    "path": "../audio/Aereda.mp3",
+    "artist": "Don_oso005",
+    "cover": ""
+},
+{
+    "name": "After The Rain",
+    "path": "../audio/After The Rain.mp3",
+    "artist": "Don_oso005",
+    "cover": ""
+},
+{
+    "name": "Arrival",
+    "path": "../audio/Arrival.mp3",
+    "artist": "Don_oso005",
+    "cover": ""
+},
+{
+    "name": "Ascending To Heaven",
+    "path": "../audio/Ascending To Heaven.mp3",
+    "artist": "Don_oso005",
+    "cover": ""
+},
+{
+    "name": "At The Precipice",
+    "path": "../audio/At The Precipice.mp3",
+    "artist": "Don_oso005",
+    "cover": ""
+},
+]
